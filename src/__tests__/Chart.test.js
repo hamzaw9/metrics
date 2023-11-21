29bff2b1546cb4ffef08809ec640951e890b9e56
Hamza Baig <mail.hamzabaig@gmail.com>
2023-11-21 22:07:58 +0500

    Add test for CoinCard component

--- src/__tests__/Chart.test.js
+++ src/__tests__/Chart.test.js
@@ -1,5 +1,5 @@
 import { screen, render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; // Import the extend-expect module
+import '@testing-library/jest-dom/extend-expect';
 
 import Chart from '../components/Chart';
 
