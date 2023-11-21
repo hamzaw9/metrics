1c4fd955e15852b0d626483491eaea4dd960f730
Hamza Baig <mail.hamzabaig@gmail.com>
2023-11-21 23:49:23 +0500

    Initial commit

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import homeReducer from './coins/coinsSlice';
-
-const store = configureStore({
-  reducer: {
-    coins: homeReducer,
-  },
-});
-export default store;
