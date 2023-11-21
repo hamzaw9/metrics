ef116676dc67a9414ee462cb6cb288fac87f2a49
Hamza Baig <mail.hamzabaig@gmail.com>
2023-11-21 15:09:18 +0500

    Install axios and redux

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Details from './pages/Details';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Details" element={<Details />} />
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
