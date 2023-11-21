331e31db510332b67a426f23b366b64b07555eb5
Hamza Baig <mail.hamzabaig@gmail.com>
2023-11-21 19:41:43 +0500

    Refactor Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,37 @@
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+/* eslint-disable implicit-arrow-linebreak */
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import Header from '../components/Header';
+import Chart from '../components/Chart';
+import SearchInput from '../components/SearchInput';
+import CoinsList from '../components/CoinsList';
 
 const Home = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(GetCoins());
-  }, []);
-  return <h1>Home Page</h1>;
+  const { coins } = useSelector((state) => state.coins);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchChange = (query) => {
+    setSearchQuery(query);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(searchQuery.toLowerCase()));
+
+  return (
+    <div
+      data-testid="home"
+      className="h-screen md:w-1/2 lg:w-1/2 mx-auto space-y-5"
+    >
+      <Header />
+      <Chart
+        title1="CRYPTO"
+        title2="CURRENCY"
+        count={`${coins.length} coins`}
+      />
+      <SearchInput onSearchChange={handleSearchChange} />
+      <CoinsList coins={filteredCoins} />
+    </div>
+  );
 };
+
 export default Home;
